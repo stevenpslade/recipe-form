754a45430616aca89837ac28feee8dfff1fb5b95
stevenpslade <stevenpslade@gmail.com>
2017-01-08 00:04:29 -0800

    header and index blank and ready for code

--- web/index.php
+++ web/index.php
@@ -3,50 +3,17 @@
 require('../vendor/autoload.php');
 require('header.php');
 
-echo "test";
 
-// $app = new Silex\Application();
-// $app['debug'] = true;
+$app = new Silex\Application();
+$app['debug'] = true;
 
-// // Register the monolog logging service
-// $app->register(new Silex\Provider\MonologServiceProvider(), array(
-//   'monolog.logfile' => 'php://stderr',
-// ));
-
-// // Register view rendering
-// $app->register(new Silex\Provider\TwigServiceProvider(), array(
-//     'twig.path' => __DIR__.'/views',
-// ));
-
-// // $dbopts = parse_url(getenv('DATABASE_URL'));
-// // $app->register(new Herrera\Pdo\PdoServiceProvider(),
-// //                array(
-// //                    'pdo.dsn' => 'pgsql:dbname='.ltrim($dbopts["path"],'/').';host='.$dbopts["host"] . ';port=' . $dbopts["port"],
-// //                    'pdo.username' => $dbopts["user"],
-// //                    'pdo.password' => $dbopts["pass"]
-// //                )
-// // );
+// Register the monolog logging service
+$app->register(new Silex\Provider\MonologServiceProvider(), array(
+  'monolog.logfile' => 'php://stderr',
+));
 
 // // Our web handlers!
 
-// $app->get('/', function() use($app) {
-//   $app['monolog']->addDebug('logging output.');
-//   return $app['twig']->render('index.twig');
-// });
-
-// // $app->get('/db/', function() use($app) {
-// //   $st = $app['pdo']->prepare('SELECT name FROM test_table');
-// //   $st->execute();
-
-// //   $names = array();
-// //   while ($row = $st->fetch(PDO::FETCH_ASSOC)) {
-// //     $app['monolog']->addDebug('Row ' . $row['name']);
-// //     $names[] = $row;
-// //   }
-
-// //   return $app['twig']->render('database.twig', array(
-// //     'names' => $names
-// //   ));
-// // });
-
 // $app->run();
+
+echo "test";
