6a72d35084e1941709f2cbc549d15492ab4a5ae3
stevenpslade <stevenpslade@gmail.com>
2017-01-08 10:34:59 -0800

    adding icons

--- web/header.php
+++ web/header.php
@@ -5,6 +5,8 @@
       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.8/css/materialize.min.css">
       <link rel="stylesheet" type="text/css" href="/stylesheets/main.css" />
 
+      <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+
       <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
     </head>
     <body>
