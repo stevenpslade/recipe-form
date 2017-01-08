c7e8c2fa76b52bcaa26fa592dded786aca253c71
stevenpslade <stevenpslade@gmail.com>
2017-01-07 23:50:23 -0800

    testing removing twig stuff

--- web/index.php
+++ web/index.php
@@ -2,48 +2,50 @@
 
 require('../vendor/autoload.php');
 
-$app = new Silex\Application();
-$app['debug'] = true;
+echo "test";
 
-// Register the monolog logging service
-$app->register(new Silex\Provider\MonologServiceProvider(), array(
-  'monolog.logfile' => 'php://stderr',
-));
+// $app = new Silex\Application();
+// $app['debug'] = true;
 
-// Register view rendering
-$app->register(new Silex\Provider\TwigServiceProvider(), array(
-    'twig.path' => __DIR__.'/views',
-));
+// // Register the monolog logging service
+// $app->register(new Silex\Provider\MonologServiceProvider(), array(
+//   'monolog.logfile' => 'php://stderr',
+// ));
 
-// $dbopts = parse_url(getenv('DATABASE_URL'));
-// $app->register(new Herrera\Pdo\PdoServiceProvider(),
-//                array(
-//                    'pdo.dsn' => 'pgsql:dbname='.ltrim($dbopts["path"],'/').';host='.$dbopts["host"] . ';port=' . $dbopts["port"],
-//                    'pdo.username' => $dbopts["user"],
-//                    'pdo.password' => $dbopts["pass"]
-//                )
-// );
+// // Register view rendering
+// $app->register(new Silex\Provider\TwigServiceProvider(), array(
+//     'twig.path' => __DIR__.'/views',
+// ));
 
-// Our web handlers!
+// // $dbopts = parse_url(getenv('DATABASE_URL'));
+// // $app->register(new Herrera\Pdo\PdoServiceProvider(),
+// //                array(
+// //                    'pdo.dsn' => 'pgsql:dbname='.ltrim($dbopts["path"],'/').';host='.$dbopts["host"] . ';port=' . $dbopts["port"],
+// //                    'pdo.username' => $dbopts["user"],
+// //                    'pdo.password' => $dbopts["pass"]
+// //                )
+// // );
 
-$app->get('/', function() use($app) {
-  $app['monolog']->addDebug('logging output.');
-  return $app['twig']->render('index.twig');
-});
+// // Our web handlers!
 
-// $app->get('/db/', function() use($app) {
-//   $st = $app['pdo']->prepare('SELECT name FROM test_table');
-//   $st->execute();
-
-//   $names = array();
-//   while ($row = $st->fetch(PDO::FETCH_ASSOC)) {
-//     $app['monolog']->addDebug('Row ' . $row['name']);
-//     $names[] = $row;
-//   }
-
-//   return $app['twig']->render('database.twig', array(
-//     'names' => $names
-//   ));
+// $app->get('/', function() use($app) {
+//   $app['monolog']->addDebug('logging output.');
+//   return $app['twig']->render('index.twig');
 // });
 
-$app->run();
+// // $app->get('/db/', function() use($app) {
+// //   $st = $app['pdo']->prepare('SELECT name FROM test_table');
+// //   $st->execute();
+
+// //   $names = array();
+// //   while ($row = $st->fetch(PDO::FETCH_ASSOC)) {
+// //     $app['monolog']->addDebug('Row ' . $row['name']);
+// //     $names[] = $row;
+// //   }
+
+// //   return $app['twig']->render('database.twig', array(
+// //     'names' => $names
+// //   ));
+// // });
+
+// $app->run();
